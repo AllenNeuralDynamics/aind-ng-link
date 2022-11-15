7c429308f1be96289f11832d7353c28d771c8132
Camilo Laiton <36769694+camilolaiton@users.noreply.github.com>
2022-11-14 16:02:18 -0800

    increasing versioning to fix bump version

--- src/ng_link/utils/utils.py
+++ src/ng_link/utils/utils.py
@@ -1,44 +1,43 @@
-import os
-import subprocess
 import json
-from pathlib import Path
-from typing import Optional, List, Union, Any
+import os
 import shutil
-from glob import glob
-import platform
-import numpy as np
+import subprocess
+from pathlib import Path
+from typing import Optional, Union
 
 # IO types
 PathLike = Union[str, Path]
 
-def create_folder(dest_dir:PathLike, verbose:Optional[bool]=False) -> None:
+
+def create_folder(dest_dir: PathLike, verbose: Optional[bool] = False) -> None:
     """
     Create new folders.
-    
+
     Parameters
     ------------------------
-    dest_dir: PathLike 
+    dest_dir: PathLike
         Path where the folder will be created if it does not exist.
     verbose: Optional[bool]
         If we want to show information about the folder status. Default False.
-        
+
     Raises
     ------------------------
-    OSError: 
+    OSError:
         if the folder exists.
-    
+
     """
-    
+
     if not (os.path.exists(dest_dir)):
         try:
             if verbose:
                 print(f"Creating new directory: {dest_dir}")
             os.makedirs(dest_dir)
         except OSError as e:
-            if (e.errno != os.errno.EEXIST):
+            if e.errno != os.errno.EEXIST:
                 raise
 
-def delete_folder(dest_dir:PathLike, verbose:Optional[bool]=False) -> None:
+
+def delete_folder(dest_dir: PathLike, verbose: Optional[bool] = False) -> None:
     """
     Delete a folder path.
     Parameters
@@ -47,18 +46,18 @@ def delete_folder(dest_dir:PathLike, verbose:Optional[bool]=False) -> None:
         Path that will be removed.
     verbose: Optional[bool]
         If we want to show information about the folder status. Default False.
-        
+
     Raises
     ------------------------
     shutil.Error:
         If the folder could not be removed.
-    
+
     Returns
     ------------------------
         None
-    
+
     """
-    if (os.path.exists(dest_dir)):
+    if os.path.exists(dest_dir):
         try:
             shutil.rmtree(dest_dir)
             if verbose:
@@ -66,36 +65,39 @@ def delete_folder(dest_dir:PathLike, verbose:Optional[bool]=False) -> None:
         except shutil.Error as e:
             print(f"Folder could not be removed! Error {e}")
 
+
 def execute_command_helper(
-        command:str, 
-        print_command:bool=False, 
-        stdout_log_file:Optional[PathLike]=None
-    ) -> None:
-    
+    command: str,
+    print_command: bool = False,
+    stdout_log_file: Optional[PathLike] = None,
+) -> None:
+
     """
     Execute a shell command.
-    
+
     Parameters
     ------------------------
     command: str
         Command that we want to execute.
     print_command: bool
         Bool that dictates if we print the command in the console.
-        
+
     Raises
     ------------------------
-    CalledProcessError: 
+    CalledProcessError:
         if the command could not be executed (Returned non-zero status).
-    
+
     """
-    
+
     if print_command:
         print(command)
-    
+
     if stdout_log_file and len(str(stdout_log_file)):
         save_string_to_txt("$ " + command, stdout_log_file, "a")
-    
-    popen = subprocess.Popen(command, stdout=subprocess.PIPE, universal_newlines=True, shell=True)
+
+    popen = subprocess.Popen(
+        command, stdout=subprocess.PIPE, universal_newlines=True, shell=True
+    )
     for stdout_line in iter(popen.stdout.readline, ""):
         yield str(stdout_line).strip()
     popen.stdout.close()
@@ -103,64 +105,63 @@ def execute_command_helper(
     if return_code:
         raise subprocess.CalledProcessError(return_code, command)
 
-def execute_command(
-        config:dict
-    ) -> None:
+
+def execute_command(config: dict) -> None:
     """
     Execute a shell command with a given configuration.
-    
+
     Parameters
     ------------------------
     command: str
         Command that we want to execute.
     print_command: bool
         Bool that dictates if we print the command in the console.
-        
+
     Raises
     ------------------------
-    CalledProcessError: 
+    CalledProcessError:
         if the command could not be executed (Returned non-zero status).
-    
+
     """
 
     for out in execute_command_helper(
-        config['command'], config['verbose'], config['stdout_log_file']
+        config["command"], config["verbose"], config["stdout_log_file"]
     ):
         if len(out):
-            config['logger'].info(out)
-        
-        if config['exists_stdout']:
-            save_string_to_txt(out, config['stdout_log_file'], "a")
- 
+            config["logger"].info(out)
+
+        if config["exists_stdout"]:
+            save_string_to_txt(out, config["stdout_log_file"], "a")
+
+
 def check_path_instance(obj: object) -> bool:
     """
     Checks if an objects belongs to pathlib.Path subclasses.
-    
+
     Parameters
     ------------------------
     obj: object
         Object that wants to be validated.
-    
+
     Returns
     ------------------------
     bool:
         True if the object is an instance of Path subclass, False otherwise.
     """
-    
+
     for childclass in Path.__subclasses__():
         if isinstance(obj, childclass):
             return True
-        
+
     return False
 
+
 def save_dict_as_json(
-        filename:str, 
-        dictionary:dict, 
-        verbose:Optional[bool]=False
-    ) -> None:
+    filename: str, dictionary: dict, verbose: Optional[bool] = False
+) -> None:
     """
     Saves a dictionary as a json file.
-    
+
     Parameters
     ------------------------
     filename: str
@@ -169,68 +170,68 @@ def save_dict_as_json(
         Dictionary that will be saved as json.
     verbose: Optional[bool]
         True if you want to print the path where the file was saved.
-        
+
     """
-    
-    if dictionary == None:
+
+    if dictionary is None:
         dictionary = {}
-    
+
     else:
         for key, value in dictionary.items():
             # Converting path to str to dump dictionary into json
             if check_path_instance(value):
                 # TODO fix the \\ encode problem in dump
                 dictionary[key] = str(value)
-    
+
     with open(filename, "w") as json_file:
         json.dump(dictionary, json_file, indent=4)
-    
+
     if verbose:
         print(f"- Json file saved: {filename}")
-        
-def read_json_as_dict(
-        filepath:str
-    ) -> dict:
-    
+
+
+def read_json_as_dict(filepath: str) -> dict:
+
     """
     Reads a json as dictionary.
-    
+
     Parameters
     ------------------------
     filepath: PathLike
         Path where the json is located.
-        
+
     Returns
     ------------------------
     dict:
         Dictionary with the data the json has.
-        
+
     """
-    
+
     dictionary = None
-    
+
     if os.path.exists(filepath):
         with open(filepath) as json_file:
             dictionary = json.load(json_file)
-        
+
     return dictionary
 
-def save_string_to_txt(txt:str, filepath:PathLike, mode='w') -> None:
+
+def save_string_to_txt(txt: str, filepath: PathLike, mode="w") -> None:
     """
     Saves a text in a file in the given mode.
-    
+
     Parameters
     ------------------------
     txt: str
         String to be saved.
-        
+
     filepath: PathLike
         Path where the file is located or will be saved.
-        
+
     mode: str
         File open mode.
-    
+
     """
-    
+
     with open(filepath, mode) as file:
         file.write(txt + "\n")
