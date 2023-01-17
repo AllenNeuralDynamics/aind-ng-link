f9fd2862831fd4ed4a6fe33eccc0b9217837ef50
Camilo Laiton <36769694+camilolaiton@users.noreply.github.com>
2023-01-17 10:56:41 -0800

    adding empty tests files

---
+++ tests/test_ng_layer.py
@@ -0,0 +1,13 @@
+"""Tests ng layer class methods."""
+
+import unittest
+
+
+class NgLayerTest(unittest.TestCase):
+    """Tests ng layer class methods."""
+
+    pass
+
+
+if __name__ == "__main__":
+    unittest.main()
