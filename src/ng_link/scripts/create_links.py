b850f29d1088765881395d7ee455257823db69fe
jwong-nd <jonathan.wong@alleninstitute.org>
2023-05-08 11:59:15 -0700

    resolve linting

--- src/ng_link/scripts/create_links.py
+++ src/ng_link/scripts/create_links.py
@@ -6,7 +6,7 @@ from ng_link import raw_link
 def main():
     # Fill in your own data
     base_channel_path = "/Users/jonathan.wong/Projects/aind-ng-link/tester.xml"
-    cross_channel_path = "/Users/jonathan.wong/Projects/aind-ng-link/registered_tester_coreg.xml"
+    # cross_channel_path = "/Users/jonathan.wong/Projects/aind-ng-link/registered_tester_coreg.xml"
     s3_path = (
         "s3://aind-open-data/diSPIM_647459_2022-12-21_00-39-00/diSPIM.zarr"
     )
