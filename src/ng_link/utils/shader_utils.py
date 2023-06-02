552b2d3b469cf23ada6660f2509bcebc92b8e0fa
danielsf <scott.daniel@alleninstitute.org>
2023-06-02 11:35:10 -0700

    add utility to create RGB shader

--- src/ng_link/utils/shader_utils.py
+++ src/ng_link/utils/shader_utils.py
@@ -1,3 +1,6 @@
+from typing import Tuple
+
+
 def create_monochrome_shader(
         color: str,
         emitter: str,
@@ -40,3 +43,42 @@ def create_monochrome_shader(
     shader_string += emit_color
 
     return shader_string
+
+
+def create_rgb_shader(
+        r_range: Tuple[int, int],
+        g_range: Tuple[int, int],
+        b_range: Tuple[int, int]) -> str:
+    """
+    Return shader code for an RGB image with different dynamic
+    ranges for each channel.
+
+    Parameters
+    ----------
+    r_range: Tuple[int, int]
+        Dynamic range of the R channel (min, max)
+    g_range: Tuple[int, int]
+        Dynamic range of the G channel (min, max)
+    b_range: Tuple[int, int[
+        Dynamic range of the B channel (min, max)
+
+    Returns
+    -------
+    code: str
+        String containing the shader code for a neuroglancer
+        RGB image.
+    """
+
+    code = "#uicontrol invlerp "
+    code += f"normalized_r(range=[{r_range[0]}, {r_range[1]}])\n"
+    code += "#uicontrol invlerp "
+    code += f"normalized_g(range=[{g_range[0]}, {g_range[1]}])\n"
+    code += "#uicontrol invlerp "
+    code += f"normalized_b(range=[{b_range[0]}, {b_range[1]}])\n"
+    code += "void main(){\n"
+    code += "float r = normalized_r(getDataValue(0));\n"
+    code += "float g = normalized_g(getDataValue(1));\n"
+    code += "float b = normalized_b(getDataValue(2));\n"
+    code += "emitRGB(vec3(r, g, b));\n}\n"
+
+    return code
--- tests/test_shader_utils.py
+++ tests/test_shader_utils.py
@@ -1,5 +1,6 @@
 from ng_link.utils.shader_utils import (
-    create_monochrome_shader)
+    create_monochrome_shader,
+    create_rgb_shader)
 
 
 def test_monochrome_shader():
@@ -16,5 +17,27 @@ def test_monochrome_shader():
     expected += 'void main() {\n'
     expected += 'emitRGB(color * normalized());\n'
     expected += '}'
+
+    assert result == expected
+
+
+def test_rgb_shader():
+    """
+    Test that create_rgb_shader runs
+    """
+    result = create_rgb_shader(
+        r_range=(1, 9),
+        g_range=(18, 37),
+        b_range=(0, 100))
+
+    expected = "#uicontrol invlerp "
+    expected += "normalized_r(range=[1, 9])\n"
+    expected += "#uicontrol invlerp normalized_g(range=[18, 37])\n"
+    expected += "#uicontrol invlerp normalized_b(range=[0, 100])\n"
+    expected += "void main(){\n"
+    expected += "float r = normalized_r(getDataValue(0));\n"
+    expected += "float g = normalized_g(getDataValue(1));\n"
+    expected += "float b = normalized_b(getDataValue(2));\n"
+    expected += "emitRGB(vec3(r, g, b));\n}\n"
 
     assert result == expected
